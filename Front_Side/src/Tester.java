606152cef3f5e775eb55193bcd57bfa5adf19896
hung-hsiang1214 <108306094@nccu.edu.tw>
2020-11-29 19:41:00 +0800

    update tester

---
+++ Front_Side/src/Tester.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class Tester {
+	public static void main(String args[]) {
+		JFrame start_frame=new Start_Frame();
+		start_frame.setDefaultCloseOperation(start_frame.EXIT_ON_CLOSE);
+		start_frame.setVisible(true);
+	}
+}
